f63a77180b065f19db13882ce282f5bc202f8a82
Thiago <lucimara1422@gmail.com>
2023-08-01 18:53:22 -0300

    vercel.json

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import 'dotenv/config'
 import mongoose from 'mongoose';
-import { ApolloServer } from 'apollo-server';
-
+import { ApolloServer } from 'apollo-server-express';
+import http from "http";
 import { typeDefs } from './typeDefs';
 import { resolvers } from './resolvers';
 import { Movie as MovieModel } from './models/movie';
 import Movies from './dataSources/movies';
+import express from 'express'
 
 const uri = process.env.MONGODB_URI
 const main = async () => {
   await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 };
-
+const app = express()
+app.use(express.json());
+const httpServer = http.createServer(app);
 main()
   .then(console.log('🎉 connected to database successfully'))
   .catch(error => console.error(error));
@@ -20,8 +23,20 @@ const dataSources = () => ({
   movies: new Movies(MovieModel),
 });
 
-const server = new ApolloServer({ typeDefs, resolvers, dataSources })
+const startApolloServer = async(app) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources
+  });
+  await server.start()
+ server.applyMiddleware({ app });
+ 
+}
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+startApolloServer(app, httpServer)
+
+
+app.listen(3000, () => console.log('server running'))
+
+export default httpServer;
