da1a11f0552487becdae8d0d38c4b505f65fcc7c
Thiago <lucimara1422@gmail.com>
2023-08-02 10:59:53 -0300

    add passwordscript

--- src/modules/User/UserModel.ts
+++ src/modules/User/UserModel.ts
@@ -1,8 +1,11 @@
 import { Maybe } from 'graphql/jsutils/Maybe'
 import mongoose, { Document, Types } from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 export interface User extends Document {
   username: string
+  password: string
+  encryptPassword: (password: string | undefined) => string
   _id: Types.ObjectId
 }
 
@@ -15,13 +18,36 @@ const UserSchema = new mongoose.Schema<User>(
       required: true,
       min: 3,
       max: 30,
+    },
+    password: {
+      required: true,
+      type: String,
+      minlength: 6,
+      hidden: true,
     }
 },
+  
   {
     collection: 'User',
     timestamps: true,
   },
 )
+
+UserSchema.pre<UserDocument>('save', function encryptPasswordHook(next) {
+  if (this.isModified('password')) {
+    this.password = this.encryptPassword(this.password)
+  }
+  return next()
+})
+
+UserSchema.methods = {
+  authenticate(password: string) {
+    return bcrypt.compareSync(password, this.password)
+  },
+  encryptPassword(password: string) {
+    return bcrypt.hashSync(password, 8)
+  },
+}
 
 export const UserModel = mongoose.model<UserDocument>('User', UserSchema)
 
