5f7036fe3ce57bbe45ecb2aa2df87d8a1af23236
Thiago <lucimara1422@gmail.com>
2023-08-04 10:08:06 -0300

    feat(deploy) example

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,12 @@
 import 'dotenv/config'
 import mongoose from 'mongoose';
-import { ApolloServer } from 'apollo-server-express';
 import http from "http";
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
-import { typeDefs } from './typeDefs';
-import { resolvers } from './resolvers';
-import { Movie as MovieModel } from './models/movie';
-import Movies from './dataSources/movies';
-import express from 'express'
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import cors from 'kcors';
 import { graphqlHTTP } from 'koa-graphql';
 import Router from 'koa-router';
-import logger from 'koa-logger';
 import { schema } from './schema/schema';
-import KoaPlayground from 'graphql-playground-middleware-koa';
 
 const uri = process.env.MONGODB_URI
 const main = async () => {
@@ -24,11 +15,10 @@ const main = async () => {
 const app = new Koa();
 const router = new Router();
 main()
-  .then(console.log('🎉 connected to database successfully'))
+  .then(() => console.log('🎉 connected to database successfully'))
   .catch(error => console.error(error));
 
-
-const graphQLSettingsPerReq = async (_req, _res, ctx) => {
+const graphQLSettingsPerReq = async (_req, _res) => {
 
   return{
     graphiql: true,
@@ -37,24 +27,19 @@ const graphQLSettingsPerReq = async (_req, _res, ctx) => {
   }
 }
 
-
 const graphQlServer = graphqlHTTP(graphQLSettingsPerReq);
 router.all('/graphql', graphQlServer);
-router.all('/graphql', KoaPlayground({ endpoint: '/'}) )
 
 app.use(cors({ credentials: true }));
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
- 
-
 const server = http.createServer(app.callback());
 	
 	server.listen(3000, () => {
-		// eslint-disable-next-line
 		console.log("server is running");
-	});
+});
 
 
 export default app;
